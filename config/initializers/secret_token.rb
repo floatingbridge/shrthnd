fbd53de183b41ba91b46893fa9b08b9debd6246b
Andres Marino <andresamarino@gmail.com>
2012-06-19 15:31:31 -0400

    inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shrthnd::Application.config.secret_token = '9173e9d0e6d8d238851226530b7e75f1bec3ea01d94da823bdcd8a097a6687cf37f9c9d8808682bd5233459eb43864febe0a28a43d1aeaf3210da5caf36ddf92'
